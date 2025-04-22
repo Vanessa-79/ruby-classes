720bcafc490e398f2607619e15dec34aaae6b03e
Vanessa <nassangavanessa2@gmail.com>
2025-04-22 18:21:40 +0300

    create an empty hash

--- substring-project/substrings_assignement.rb
+++ substring-project/substrings_assignement.rb
@@ -4,7 +4,11 @@ Implement a method #substrings that takes a word as the first argument and then 
 
 
 # define method
-def substrings(word, dictionary)
+def substrings(input, dictionary)
+  input = input.downcase
+  words = input.split
   
+  # create a hash to store the results with a default value 0
+  result = Hash.new(0)
   
 end
